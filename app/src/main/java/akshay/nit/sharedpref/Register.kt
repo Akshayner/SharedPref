22905c99a6b65ef724d23c0252eb97fce037d2a9
nit <akshay.gurnule@gmail.com>
2018-07-23 10:53:36 +0530

    new

--- app/src/main/java/akshay/nit/sharedpref/Register.kt
+++ app/src/main/java/akshay/nit/sharedpref/Register.kt
@@ -7,25 +7,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.register.*
+import kotlinx.android.synthetic.main.register.view.*
 
 class Register:Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var v = inflater.inflate(R.layout.register,container,false)
-
-        var spf = activity!!.getSharedPreferences("mypref",Context.MODE_PRIVATE)
-        var spe = spf.edit()
-        spe.putString("uname",et1.text.toString())
-        spe.putString("pass",et2.text.toString())
-        spe.putString("email",et3.text.toString())
-        spe.putString("mob",et4.text.toString())
-        spe.commit()
-
-        var fm = activity!!.supportFragmentManager
-        var tx = fm.beginTransaction()
-        tx.replace(R.id.frag,Login())
-        tx.commit()
+        var v = inflater.inflate(R.layout.register, container, false)
 
 
-        return v
+        v.reg.setOnClickListener {
+            var spf = activity!!.getSharedPreferences("mypref", Context.MODE_PRIVATE)
+            var spe = spf.edit()
+            spe.putString("uname", et1.text.toString())
+            spe.putString("pass", et2.text.toString())
+            spe.putString("email", et3.text.toString())
+            spe.putString("mob", et4.text.toString())
+            spe.commit()
+
+            var fm = activity!!.supportFragmentManager
+            var tx = fm.beginTransaction()
+            tx.replace(R.id.frag, Login())
+            tx.commit()
+
+
+            return v
+        }
     }
 }
